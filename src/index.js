c48b2d1200f5d81bb6146aa938169e02bd81c99b
FOX-P1 <smgms7513@gmail.com>
2022-12-27 20:04:58 +0900

    chapter_15_fin

--- src/index.js
+++ src/index.js
@@ -10,14 +10,18 @@ import "./index.css";
 // import Accommodate from "./chapter_07/Accommodate";
 // import ConfirmButton from "./chapter_08/ConfirmButton";
 // import LandingPage from "./chapter_09/LandingPage";
-import AttendanceBook from "./chapter_10/AttendanceBook";
+// import AttendanceBook from "./chapter_10/AttendanceBook";
+// import SignUp from "./chapter_11/SignUp";
+// import Calculator from "./chapter_12/Calculator";
+// import ProfileCard from "./chapter_13/ProfileCard";
+import DarkOrLight from "./chapter_14/DarkOrLight";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AttendanceBook />
+        <DarkOrLight />
     </React.StrictMode>
 );
 
