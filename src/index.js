01ea587646df8e691380fa8f9cc267c4f46048c8
FOX-P1 <smgms7513@gmail.com>
2022-12-27 20:06:54 +0900

    chapter_15_fin_2

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ import "./index.css";
 // import SignUp from "./chapter_11/SignUp";
 // import Calculator from "./chapter_12/Calculator";
 // import ProfileCard from "./chapter_13/ProfileCard";
-import DarkOrLight from "./chapter_14/DarkOrLight";
+// import DarkOrLight from "./chapter_14/DarkOrLight";
+import Blocks from "./chapter_15/Blocks";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <DarkOrLight />
+        <Blocks />
     </React.StrictMode>
 );
 
